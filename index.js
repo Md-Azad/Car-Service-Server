af90c452c960ddd831df5915674e6f1dd89f5cb5
Md-Azad <sokalazad@gmail.com>
2023-12-05 22:00:44 +0100

    created booking api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,11 +27,31 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const serviceCollection = client.db("CarService").collection("Services");
+    const bookingCollection = client.db("CarService").collection("bookings");
 
     app.get('/services',async(req,res)=>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+
+    app.get('/services/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const options = {
+        projection: {service_id:1, title:1,price:1,img:1},
+      }
+      const result = await serviceCollection.findOne(query,options);
+      res.send(result);
+    })
+
+    // booking related api
+
+    app.post('/booking',async(req,res)=>{
+      const booking = req.body;
+      // console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
