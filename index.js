51fd76f1939cee688d1e14a892a6480f952eb156
Md-Azad <sokalazad@gmail.com>
2023-12-06 01:07:06 +0100

    created patch api

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cn37c5v.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,52 +27,66 @@ async function run() {
     const serviceCollection = client.db("CarService").collection("Services");
     const bookingCollection = client.db("CarService").collection("bookings");
 
-    app.get('/services',async(req,res)=>{
-        const cursor = serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-
-    app.get('/services/:id',async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const options = {
-        projection: {service_id:1, title:1,price:1,img:1},
-      }
-      const result = await serviceCollection.findOne(query,options);
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
       res.send(result);
-    })
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { service_id: 1, title: 1, price: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
     // booking related api
-    app.get('/booking',async(req,res)=>{
+    app.get("/booking", async (req, res) => {
       let query = {};
       console.log(req.query.email);
-      if(req.query?.email){
-        query = {email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
       // above query will give us booking data according to user email;
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
-    app.post('/booking',async(req,res)=>{
+    app.post("/booking", async (req, res) => {
       const booking = req.body;
       // console.log(booking);
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
-    })
+    });
 
-    app.delete('/booking/:id',async(req,res)=>{
+    app.patch("/booking/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const updateBooking = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -82,11 +94,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("car-service is running");
+});
 
-app.get('/',(req,res)=>{
-    res.send("car-service is running");
-})
-
-app.listen(port,()=>{
-    console.log(`car service server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`car service server is running on port ${port}`);
+});
